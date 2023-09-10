77a33cb6ab1bdbd67837f7520c2a2cd69a8b3ebe
Swan-Cai <96821426+Swan-Cai@users.noreply.github.com>
2023-09-10 19:16:29 +0100

    Initial commit

---
+++ kafka-consumer-database/src/main/java/me/wanpin/springboot/entity/WikimediaData.java
@@ -0,0 +1,21 @@
+package me.wanpin.springboot.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "wikimedia_recentchange")
+@Getter
+@Setter
+public class WikimediaData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String wikiEventData;
+}
---
+++ kafka-consumer-database/src/main/java/me/wanpin/springboot/repository/WikimediaDataRepository.java
@@ -0,0 +1,7 @@
+package me.wanpin.springboot.repository;
+
+import me.wanpin.springboot.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData,Long> {
+}
